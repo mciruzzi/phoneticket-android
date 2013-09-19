d8c8e64226a43d45708f22b2afb8a13ff3df227b
mciruzzi <m.ciruzzi@hotmail.com>
2013-09-19 17:57:13 -0300

    Primer navegacion a Activity de Peliculas

--- phoneticket-app/src/main/java/com/cinemar/phoneticket/MainMenuActivity.java
+++ phoneticket-app/src/main/java/com/cinemar/phoneticket/MainMenuActivity.java
@@ -1,25 +1,47 @@
 package com.cinemar.phoneticket;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainMenuActivity extends Activity {
-
+	//Referencias a UI
+	private TextView welcomeView; 
+	private ImageButton peliculasButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_main_menu);
+		
 		String userName = getIntent().getStringExtra("userId");
 				
-	    // Create the text view
-	    TextView welcomeView = new TextView(this);
-	    welcomeView.setTextSize(30);
-	    welcomeView.setText("Hola " + userName);
+		welcomeView = (TextView) findViewById(R.id.welcome_message);
+		welcomeView.setVisibility(View.VISIBLE);
+	    welcomeView.setText("Hola " + userName);		    
+	    
+	    peliculasButton = (ImageButton) findViewById(R.id.peliculasButton); 
+	    peliculasButton.setOnClickListener(
+				new View.OnClickListener() {
+					public void onClick(View view) {
+						goToPeliculasActivity();
+					}
+				});		
+		
+	}
 
-		setContentView(welcomeView);
-		
-		
+	protected void goToPeliculasActivity() {
+		Intent intent = new Intent(this, PeliculasActivity.class);
+		//TODO put extra content to peliculas Intent
+		//Podria ser la sala seleccionada, si es que ya fue seleccionada
+		startActivity(intent);		
 	}
 
 	@Override
