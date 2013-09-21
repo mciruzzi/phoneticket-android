576e1e1f1af22f5d3934d414d17ac94a6eb0da21
Juan Manuel Barreneche <snipperme@gmail.com>
2013-09-21 15:56:01 -0300

    Set main menu as main activity

--- phoneticket-app/src/main/java/com/cinemar/phoneticket/MainMenuActivity.java
+++ phoneticket-app/src/main/java/com/cinemar/phoneticket/MainMenuActivity.java
@@ -2,46 +2,61 @@ package com.cinemar.phoneticket;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainMenuActivity extends Activity {
 	//Referencias a UI
-	private TextView welcomeView; 
+	private TextView welcomeView;
 	private ImageButton peliculasButton;
-	
+	private Button miCuentaButton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_main_menu);
-		
-		String userName = getIntent().getStringExtra("userId");
-				
+
 		welcomeView = (TextView) findViewById(R.id.welcome_message);
-		welcomeView.setVisibility(View.VISIBLE);
-	    welcomeView.setText("Hola " + userName);		    
-	    
-	    peliculasButton = (ImageButton) findViewById(R.id.peliculasButton); 
-	    peliculasButton.setOnClickListener(
+		String userName = getIntent().getStringExtra("userId");
+
+		if (userName != null) {
+			welcomeView.setText("Hola " + userName);
+		} else {
+			welcomeView.setText("Bienvenido");
+		}
+
+		peliculasButton = (ImageButton) findViewById(R.id.peliculasButton);
+		peliculasButton.setOnClickListener(
 				new View.OnClickListener() {
 					public void onClick(View view) {
 						goToPeliculasActivity();
 					}
-				});		
-		
+				});
+		miCuentaButton = (Button) findViewById(R.id.miCuentaButton);
+		miCuentaButton.setOnClickListener(
+				new View.OnClickListener() {
+					public void onClick(View view) {
+						goToLoginActivity();
+					}
+
+				});
 	}
 
 	protected void goToPeliculasActivity() {
 		Intent intent = new Intent(this, PeliculasActivity.class);
 		//TODO put extra content to peliculas Intent
 		//Podria ser la sala seleccionada, si es que ya fue seleccionada
-		startActivity(intent);		
+		startActivity(intent);
+	}
+
+	protected void goToLoginActivity() {
+		Intent intent = new Intent(this, LoginActivity.class);
+		startActivity(intent);
 	}
 
 	@Override
