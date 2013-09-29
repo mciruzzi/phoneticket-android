44f983bd1902ed54eb298612fc29b97cc6468825
AmaliaMV <amaliavillanueva@gmail.com>
2013-09-29 00:01:08 -0300

    vista del editar datos

---
+++ phoneticket-app/src/main/java/com/cinemar/phoneticket/MyAccountActivity.java
@@ -0,0 +1,22 @@
+package com.cinemar.phoneticket;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MyAccountActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my_account);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.my_account, menu);
+		return true;
+	}
+
+}
