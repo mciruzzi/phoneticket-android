c10c7ca9f92809fa00818c26814a9f6f6b4b3b79
AmaliaMV <amaliavillanueva@gmail.com>
2013-11-16 16:56:48 -0300

    asientos ordenados en los detalles

--- phoneticket-app/src/main/java/com/cinemar/phoneticket/reserveandbuy/ReserveRequest.java
+++ phoneticket-app/src/main/java/com/cinemar/phoneticket/reserveandbuy/ReserveRequest.java
@@ -1,13 +1,15 @@
 package com.cinemar.phoneticket.reserveandbuy;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class ReserveRequest {
 	
 	String email;
 	String showId;
-	Collection<String> seatsIds; 
+	List<String> seatsIds; 
 	int seatsCount;
 
 	public String getEmail() {
@@ -22,14 +24,21 @@ public class ReserveRequest {
 	public void setShowId(String showId) {
 		this.showId = showId;
 	}
-	public Collection<String> getSeats() {
+	public List<String> getSeats() {
 		return seatsIds;
 	}
 	public ArrayList<String> getSeatsAsArray() {
 		ArrayList<String> result = new ArrayList<String>(seatsIds);
 		return result;
 	}
-	public void setSeats(Collection<String> seatsIds) {
+	public void setSeats(List<String> seatsIds) {
+	Collections.sort(seatsIds, new Comparator<String>()
+            {
+                public int compare(String f1, String f2)
+                {
+                    return f2.toString().compareTo(f1.toString());
+                }        
+            });
 		this.seatsIds = seatsIds;
 		seatsCount = seatsIds.size();
 	}	
